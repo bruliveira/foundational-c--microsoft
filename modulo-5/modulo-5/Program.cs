1e2a024e8e3db55a4363c91d00036c66e1d6bd20
bruliveira <brunaflavia910@gmail.com>
2024-01-03 21:35:40 -0300

    :goat: Module 5

---
+++ modulo-5/modulo-5/Program.cs
@@ -0,0 +1,582 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Parte 1: Primeiro método");
+//#region "Metodo base"
+//int[] numerosMetodo = { 1, 2, 3, 4, 5 };
+//Console.WriteLine("Valores array: ");
+
+//SorteioNumerosAleatorios();
+
+//void PrimeiroMetodo()
+//{
+//    Console.WriteLine("Hello World");
+//}
+//void PrintArray()
+//{
+//    foreach (int x in numerosMetodo)
+//    {
+//        Console.WriteLine($"{x}");
+//    }
+//    Console.WriteLine();
+//}
+//void SorteioNumerosAleatorios()
+//{
+//    Random random = new Random();
+
+//    for (int i = 0; i < 5; i++)
+//    {
+//        Console.Write($"{random.Next(1, 100)} ");
+//    }
+
+//    Console.WriteLine();
+//}
+//#endregion
+
+//#region "Metodos reutilizaveis"
+//int[] times = { 800, 1200, 1600, 2000 };
+//int diff = 0;
+
+//Console.WriteLine("Enter current GMT");
+//int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Current Medicine Schedule:");
+
+///* Format and display medicine times */
+//DisplayTimes();
+
+//Console.WriteLine("Enter new GMT");
+//int newGMT = Convert.ToInt32(Console.ReadLine());
+
+//if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//{
+//    Console.WriteLine("Invalid GMT");
+//}
+//else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//{
+//    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+
+//    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//   AdjustTimes();   
+//}
+//else
+//{
+//    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+//    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//    AdjustTimes();
+//}
+
+//Console.WriteLine("New Medicine Schedule:");
+
+///* Format and display medicine times */
+//DisplayTimes();
+
+//void DisplayTimes()
+//{
+//    /* Format and display medicine times */
+//    foreach (int val in times)
+//    {
+//        string time = val.ToString();
+//        int len = time.Length;
+
+//        if (len >= 3)
+//        {
+//            time = time.Insert(len - 2, ":");
+//        }
+//        else if (len == 2)
+//        {
+//            time = time.Insert(0, "0:");
+//        }
+//        else
+//        {
+//            time = time.Insert(0, "0:0");
+//        }
+
+//        Console.Write($"{time} ");
+//    }
+
+//}
+//void AdjustTimes()
+//{
+//    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//    for (int i = 0; i < times.Length; i++)
+//    {
+//        times[i] = ((times[i] + diff)) % 2400;
+//    }
+//}
+//#endregion
+
+//#region "Exercicio - Compilar codigo com metodos"
+//string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
+//string[] address;
+//bool validLength = false;
+//bool validZeroes = false;
+//bool validRange = false;
+
+//foreach (string ip in ipv4Input)
+//{
+//    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+//    ValidateLength();
+//    ValidateZeroes();
+//    ValidateRange();
+
+//    if (validLength && validZeroes && validRange)
+//    {
+//        Console.WriteLine($"{ip} is a valid IPv4 address");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{ip} is an invalid IPv4 address");
+//    }
+//}
+//void ValidateLength() 
+//{
+//    validLength = address.Length == 4;
+//}
+//void ValidateZeroes()
+//{
+//    foreach (string number in address)
+//    {
+//        if (number.Length > 1 && number.StartsWith("0"))
+//        {
+//            validZeroes = false;
+//            return;
+//        }
+//    }
+//    validZeroes = true;
+//}
+//void ValidateRange()
+//{
+//    foreach (string number in address)
+//    {
+//        int value = int.Parse(number);
+//        if (value < 0 || value > 255)
+//        {
+//            validRange = false;
+//            return;
+//        }
+//    }
+//    validRange = true;
+//}
+
+//#endregion
+
+//#region "Desafio - Criar metodo reutilizavel"
+//Random random = new Random();
+//int luck = random.Next(100);
+
+//string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+//string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+//string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+//string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+
+//TellFortune();
+//luck = random.Next(100);
+//TellFortune();
+
+//void TellFortune()
+//{
+//    Console.WriteLine("A fortune teller whispers the following words:");
+//    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+//    for (int i = 0; i < 4; i++)
+//    {
+//        Console.Write($"{text[i]} {fortune[i]} ");
+//    }
+//}
+
+//#endregion
+
+Console.WriteLine("\nParte 2: Criar métodos com parâmetros");
+#region "Usar parâmetros em métodos"
+int[] schedule = { 800, 1200, 1600, 2000 };
+
+
+DisplayAdjustedTimes(schedule, 6, -6);
+CountTo(5);
+
+void CountTo(int max)
+{
+    for (int i = 0; i < max; i++)
+    {
+        Console.Write($"${i}, ");
+    }
+}
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+{
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid GMT");
+    }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    }
+    else
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    }
+    for (int i = 0; i < times.Length; i++)
+    {
+        int newTime = ((times[i] + diff)) % 2400;
+        Console.WriteLine($"{times[i]} -> {newTime}");
+    }
+}
+#endregion
+
+#region "Escopo do método"
+Console.WriteLine("\n");
+string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+double pi = 3.14159;
+
+
+DisplayStudents(students);
+DisplayStudents(new string[] { "Robert", "Vanya" });
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void DisplayStudents(string[] students)
+{
+    foreach (string student in students)
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
+
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+void PrintCircleInfo(int radius)
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+#endregion
+
+#region "Parâmetros de tipo de referência e de valor"
+Console.WriteLine();
+int a = 3;
+int b = 4;
+int c = 0;
+int[] array = { 1, 2, 3, 4, 5 };
+string status = "Healthy";
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+Console.WriteLine($"Start: {status}");
+SetHealth(false);
+Console.WriteLine($"End: {status}");
+
+void Multiply(int a, int b, int c)
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
+void SetHealth(bool isHealthy)
+{
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
+}
+#endregion
+
+#region "Métodos com parâmetros opcionais"
+Console.WriteLine("");
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Linh", 2, allergies: "none", inviteOnly: false); 
+RSVP("Linh", partySize: 2, "none", false);
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+
+ShowRSVPs();
+
+// void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+void RSVP(string name, int partySize = 1, string allergies = "none1", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
+
+#endregion
+
+#region "Exibir os endereços de email"
+string[,] corporate =
+    {
+        {"Robert", "Bavin"}, {"Simon", "Bright"},
+        {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+        {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+string[,] external =
+{
+        {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+        {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+    };
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(first: external[i, 0], last: external[i, 1], domain: externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
+
+#endregion
+
+Console.WriteLine("\nParte 3: Criar métodos C# que retornem valores");
+#region
+
+double total = 0;
+double minimumSpend = 30.00;
+
+double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+for (int i = 0; i < items.Length; i++)
+{
+    total += GetDiscountedPrice(i);
+}
+
+total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
+{
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+}
+
+bool TotalMeetsMinimum()
+{
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
+}
+
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd)
+{
+    int rate = 23500;
+    return (int)(rate * usd);
+}
+
+double VndToUsd(int vnd)
+{
+    double rate = 23500;
+    return vnd / rate;
+}
+#endregion
+
+#region "Retornar cadeias de caracteres de métodos"
+string input = "there are snakes at the zoo";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseSentence(string input)
+{
+    string result = "";
+    string[] words = input.Split(" ");
+    foreach (string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+    return result.Trim();
+}
+
+string ReverseWord(string word)
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+    return result;
+}
+#endregion
+
+#region "Retornar matriz de métodos"
+int target = 30;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        if (result[i, 0] == -1)
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+    }
+}
+
+int[,] TwoCoins(int[] coins, int target)
+{
+    int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0))
+            {
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0, 0] : result;
+}
+#endregion
+
+#region "Adicionar métodos a fim de tornar o jogo viável"
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+bool ShouldPlay()
+{
+    string response = Console.ReadLine();
+    return response.ToLower().Equals("y");
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = GetTarget();
+        var roll = RollDice();
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+int GetTarget()
+{
+    return random.Next(1, 6);
+}
+
+int RollDice()
+{
+    return random.Next(1, 7);
+}
+
+string WinOrLose(int roll, int target)
+{
+    if (roll > target)
+    {
+        return "You win!";
+    }
+    return "You lose!";
+}
+
+#endregion
